b9dd24551e37fce6b73165a560b8a9195fb8075c
yuanliwei <891789592@qq.com>
2019-10-11 17:00:40 +0800

    version 1.0

--- app/src/main/java/com/ylw/opendevelopmentsettings/MainActivity.java
+++ app/src/main/java/com/ylw/opendevelopmentsettings/MainActivity.java
@@ -1,14 +1,32 @@
 package com.ylw.opendevelopmentsettings;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.app.TaskStackBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        startActivity(new Intent("android.settings.APPLICATION_DEVELOPMENT_SETTINGS"));
+        finish();
+        NotificationCompat.Builder localBuilder = new NotificationCompat.Builder(this, "ylw")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(false)
+                .setColorized(true)
+                .setColor(0xff643787)
+                .setOngoing(true)
+                .setContentTitle("打开开发者选项")
+                .setContentText("😜😜😜😂😍😘☺🤩🤣");
+        TaskStackBuilder paramBundle = TaskStackBuilder.create(this);
+        paramBundle.addNextIntent(new Intent("android.settings.APPLICATION_DEVELOPMENT_SETTINGS"));
+        localBuilder.setContentIntent(paramBundle.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT));
+        NotificationManagerCompat.from(this).notify(100, localBuilder.build());
     }
 }
