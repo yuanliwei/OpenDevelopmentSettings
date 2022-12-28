9bf57f0373cbea7484cb7400497a5706b242d181
yuanliwei <891789592@qq.com>
2022-12-28 15:52:53 +0800

    update Notification

--- app/src/main/java/com/ylw/opendevelopmentsettings/MainActivity.java
+++ app/src/main/java/com/ylw/opendevelopmentsettings/MainActivity.java
@@ -1,32 +1,44 @@
 package com.ylw.opendevelopmentsettings;
 
+import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
-import androidx.core.app.TaskStackBuilder;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         startActivity(new Intent("android.settings.APPLICATION_DEVELOPMENT_SETTINGS"));
         finish();
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("ylw", "ylw", NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("ylw");
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
         NotificationCompat.Builder localBuilder = new NotificationCompat.Builder(this, "ylw")
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSmallIcon(R.drawable.ic_action)
                 .setAutoCancel(false)
+                .setSilent(true)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
                 .setColorized(true)
                 .setColor(0xff643787)
                 .setOngoing(true)
                 .setContentTitle("打开开发者选项")
                 .setContentText("😜😜😜😂😍😘☺🤩🤣");
-        TaskStackBuilder paramBundle = TaskStackBuilder.create(this);
-        paramBundle.addNextIntent(new Intent("android.settings.APPLICATION_DEVELOPMENT_SETTINGS"));
-        localBuilder.setContentIntent(paramBundle.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT));
+        TaskStackBuilder intentBuilder = TaskStackBuilder.create(this);
+        intentBuilder.addNextIntent(new Intent("android.settings.APPLICATION_DEVELOPMENT_SETTINGS"));
+        localBuilder.setContentIntent(intentBuilder.getPendingIntent(0, PendingIntent.FLAG_IMMUTABLE));
         NotificationManagerCompat.from(this).notify(100, localBuilder.build());
     }
 }
